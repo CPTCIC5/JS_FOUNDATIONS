2fa8d74092e73a74a25ac5ed768af9ef62b54ce3
ARYAN <75555287+CPTCIC5@users.noreply.github.com>
2022-02-05 00:10:24 +0530

    Add files via upload

---
+++ dom-navigation.js
@@ -0,0 +1,12 @@
+//id is unique so by 1 id assigned obj in html u can name/access 1 obj
+
+let n1=document.getElementById('heading');
+console.log(n1);
+
+let n2=document.getElementById('nav');
+console.log(n2);
+
+//class is given to multiple objects n used to access on multiple places
+
+let n3=document.getElementsByClassName('container')
+console.log(n3);
