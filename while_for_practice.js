d20a8009d4717aeecba62ae6889a9950af4dbfa8
ARYAN <75555287+CPTCIC5@users.noreply.github.com>
2022-03-17 18:57:18 +0530

    Add files via upload

---
+++ while_for_practice.js
@@ -0,0 +1,60 @@
+/*
+let tries_used=0
+let total_tries=5
+while (tries_used<total_tries){
+    tries_used+=1
+    let x1=prompt('Guess a number')
+    if (x1 == 16){
+        console.log('Won')
+        break
+    }else{
+        console.log('Sad')
+    }
+}
+console.log('Game Over')
+*/
+
+/*
+for (let tries_used=0; tries_used<5; tries_used++) {
+    let x1=prompt("Guess a no.")
+    if (x1 == 16) {
+        console.log('Won')
+        break
+    }else{
+        console.log('Sad')
+    }
+    
+}
+console.log('Game Over')
+*/
+
+
+
+/*
+let students=[];
+let marks=[];
+let n1=0
+let x1=prompt('How many students data u wanna save');
+while (n1<x1){
+    n1++
+    let s_student=prompt("What is ur name")
+    students.push(s_student)
+    let m_marks=prompt("What is ur marks")
+    marks.push(m_marks)
+}
+console.log(students,marks)
+*/
+
+
+/*
+let students=[];
+let marks=[];
+let x1=prompt('How many students data u wanna save');
+for (let n1=0; n1<x1; n1++) {
+    let s_student=prompt("What is ur name")
+    students.push(s_student)
+    let m_marks=prompt("What is ur marks")
+    marks.push(m_marks)
+}
+console.log(students,marks)
+*/
